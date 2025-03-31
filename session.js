7fc7a6548e1fdae8344b7940e1dabeb08b7f6b45
xchamo234 <xchamonirmal9@gmail.com>
2025-03-31 16:28:33 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,8 +40,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your session id here",
-SUDO: process.env.SUDO || ["94717775628","94758775628"],
+SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~yVtEzbiT#AvJxCTtk7q5o_rxT3_t7hojtYvVB-L-9o0F7kNzfZ84",
+SUDO: process.env.SUDO || ["94777580876"],
 PORT: process.env.PORT || "8000",
-FOOTER: process.env.FOOTER || "> QUEEN ANJU XPRO\n> Developed by Mr. Rashmika\n> GitHub Repository: github.com/Mrrashmika/QUEEN_ANJU_xPro\n🌟 Premium Version | Advanced Features | Secure & Reliable 🌟\n© 2025 QUEEN ANJU XPRO. All rights reserved.",
+FOOTER: process.env.FOOTER || "> ʟᴏᴋɪɴᴏ 🕊️",
 };
